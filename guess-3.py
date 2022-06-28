ccccd419d5bec5ec21d60d11971c94fbe762bd35
Oscar Vazquez <oscar.navvaz@gmail.com>
2022-06-28 14:27:48 -0700

    initial

---
+++ guess-3.py
@@ -0,0 +1,76 @@
+import random
+
+NUM_DIGITS = 3
+MAX_GUESSES = 10
+
+def main():
+    print('''
+I am thinking of a {}-digit number with no repeated digits.
+Try to guess what it is. Here are some clues:
+
+When I say:         That means:
+    Warm            One digit is correct but in the wrong position
+    Hot             One digit is correct and in the right position
+    Cold            No digit is correct
+    
+For example, if the secret number was 248 and your guess was 843,
+the clues would be Hot Warm.'''.format(NUM_DIGITS))
+
+    while True:
+        secretNum = getSecretNum()
+        print('I have thought of a number.')
+        print('You have {} guesses to get it.'.format(MAX_GUESSES))
+
+        numGuesses = 1
+        while numGuesses <= MAX_GUESSES:
+            guess = ''
+            while len(guess) != NUM_DIGITS or not guess.isdecimal():
+                print('Guess #{}: '.format(numGuesses))
+                guess = input('> ')
+            
+            clues = getClues(guess, secretNum)
+            print(clues)
+            numGuesses += 1
+
+            if guess == secretNum:
+                break
+            if numGuesses > MAX_GUESSES:
+                print("You're out of guesses.")
+                print('The answer was {}.'.format(secretNum))
+
+        print('Do you want to play again? (yes or no)')
+        if not input('> ').lower().startswith('y'):
+            break
+    print("Thanks for playing!")
+
+
+def getSecretNum():
+    numbers = list('0123456789')
+    random.shuffle(numbers)
+
+    secretNum = ''
+    for i in range(NUM_DIGITS):
+        secretNum += str(numbers[i])
+    return secretNum
+
+
+def getClues(guess, secretNum):
+    if guess == secretNum:
+        return "You got it!"
+
+    clues = []
+
+    for i in range(len(guess)):
+        if guess[i] == secretNum[i]:
+            clues.append('Hot')
+        elif guess[i] in secretNum:
+            clues.append('Warm')
+    if len(clues) == 0:
+        return 'Cold'
+    else:
+        clues.sort() 
+        return ' '.join(clues)
+
+# Run game if the program is ran instead of imported
+if __name__ == '__main__':
+    main()
